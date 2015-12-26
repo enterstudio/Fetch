3c16c83244eafcb08d9c988caa5a063c56878bcb
DigiTechs <DigiTechs@users.noreply.github.com>
2015-12-26 17:03:03 +0000

    Push local code repository and fix everything else

---
+++ README.md
@@ -0,0 +1,7 @@
+# Fetch #
+
+A Scratch 2.0 project sharing site
+
+## Talk to us ##
+
+We're on gitter! Talk to us [here](https://gitter.im/DigiTechs/Fetch)!
---
+++ Shared/Head.php
@@ -0,0 +1,14 @@
+<?php
+	require "Shared/Config.php";
+	require_once "Shared/Util/URLTransformation.php";
+?>
+<!-- HTML head from Head.php -->
+
+<!-- Materialize.css -->
+<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/css/materialize.min.css">
+
+<!-- Mark us as mobile-supporting -->
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+<link rel="stylesheet" href="<?= URL("Content/Site.css"); ?>"">
